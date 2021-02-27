d8787e2ab880356c90f2ce1094fa1908f1b16394
andrebaran <andrebarandossantos@yahoo.com.br>
2021-02-27 09:09:02 -0300

    trabalho extra

---
+++ app/src/main/java/com/example/fragmentos/StartFragment.kt
@@ -0,0 +1,44 @@
+package com.example.fragmentos
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import com.example.fragmentos.databinding.FragmentStartBinding
+
+class StartFragment : Fragment() {
+
+    private var _binding: FragmentStartBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        layoutInflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentStartBinding.inflate(layoutInflater)
+
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.botaoEsquerda.setOnClickListener {
+            val ação = StartFragmentDirections.paraEsquerda()
+            findNavController().navigate(ação)
+        }
+        binding.botaoDireita.setOnClickListener {
+            val ação = StartFragmentDirections.paraDireita()
+            findNavController().navigate(ação)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
